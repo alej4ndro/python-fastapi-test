81a06ae965cf7c0a84a4f2664b376eb1fb7a63e9
Gerard Solanes <gerardsolher@gmail.com>
2024-04-23 15:44:52 +0200

    Implements dummy practitioner data

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,10 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Query
+from ..models.practitioner import Practitioner
 
 router = APIRouter()
 
-@router.get("/health")
-def health():
+
+@router.get("/practitioner", response_model=Practitioner, responses={400: {"description": "Invalid member ID value"}})
+def get_practitioner(memberId: str = Query(..., description="Member ID to be considered for filter")):
     # Dummy data for demonstration
-    return {
-        "message": "Service is up"
-    }
+    if memberId != "valid":
+        raise HTTPException(status_code=400, detail="Invalid member ID value")
+    return Practitioner(
+        memberId="123456789",
+        fullName="José García García",
+        membershipProvince="Madrid",
+        medicalSpecialty="Médico especialista en Pediatría",
+        membershipState="ALTA",
+        workAddress="Calle Singular Nº 0"
+    )
